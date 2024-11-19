bcdb84da00255b34cfeed492e194eea27dfe25b8
Alvaro Isea <alvaro.isea@pulselive.com>
2024-11-19 11:51:36 +0100

    added numbers to weight lines and clean up

--- app/src/main/java/com/ai/customui/MainActivity.kt
+++ app/src/main/java/com/ai/customui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ai.customui
 
+import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Paint.Align
 import android.graphics.Color as ColorOld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.Px
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
@@ -14,8 +17,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,14 +28,21 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ai.customui.ui.theme.CustomUITheme
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
 import kotlin.math.pow
+import kotlin.math.sin
 import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
@@ -119,58 +127,190 @@ fun MyCanvasSquare() {
 @Composable
 fun Scale(
     modifier: Modifier = Modifier,
-    scaleStyle: ScaleStyle = ScaleStyle()
+    scaleStyle: ScaleStyle = ScaleStyle(),
+    minWeight: Int = 20,
+    maxWeight: Int = 200,
+    initialWeight: Int = 70
 ) {
     val radius = scaleStyle.radius
     val scaleWidth = scaleStyle.scaleWidth
 
     // the center of the canvas
-    var center by remember {
-        mutableStateOf(Offset.Zero)
-    }
+    var center by remember { mutableStateOf(Offset.Zero) }
 
     // The center of the scale
-    var circleCenter by remember {
-        mutableStateOf(Offset.Zero)
-    }
+    var circleCenter by remember { mutableStateOf(Offset.Zero) }
+
+    val angle by remember { mutableStateOf(0f) }
 
     Canvas(modifier = modifier) {
 
         center = this.center
 
-        circleCenter = Offset(
-            center.x,
-            scaleWidth.toPx() / 2f + radius.toPx()
+        circleCenter = Offset(x = center.x, y = scaleWidth.toPx() / 2 + radius.toPx())
+
+        //radius from circle center to outer and inner edge of the scale
+        val outerRadius = radius.toPx() + scaleWidth.toPx() / 2f
+        val innerRadius = radius.toPx() - scaleWidth.toPx() / 2f
+
+        val nativeCanvas = drawContext.canvas.nativeCanvas
+
+        // Scale and shadow around it
+        drawScale(
+            nativeCanvas = nativeCanvas,
+            circleCenter = circleCenter,
+            radiusInPx = radius.toPx(),
+            scaleWidthInPx = scaleWidth.toPx()
         )
 
-        // shadow around the scale widget
-        drawContext.canvas.nativeCanvas.apply {
-            drawCircle(
-                circleCenter.x,
-                circleCenter.y,
-                radius.toPx(),
-                Paint().apply {
-                    strokeWidth = scaleWidth.toPx()
-                    color = android.graphics.Color.WHITE
-                    setStyle(Paint.Style.STROKE)
-                    setShadowLayer(
-                        60f,
-                        0f,
-                        0f,
-                        ColorOld.argb(50,0,0,0)
-                    )
-                }
-            )
-        }
+        // lines for weight
+        drawScaleWeightLines(
+            minWeight = minWeight,
+            maxWeight = maxWeight,
+            initialWeight = initialWeight,
+            angle = angle,
+            scaleStyle = scaleStyle,
+            outerRadius = outerRadius,
+            circleCenter = circleCenter,
+            drawScope = this,
+            nativeCanvas = nativeCanvas
+        )
 
     }
+
+}
+
+private fun drawScale(
+    nativeCanvas: Canvas,
+    circleCenter: Offset,
+    radiusInPx: Float,
+    scaleWidthInPx: Float
+) {
+    nativeCanvas.apply {
+        drawCircle(
+            circleCenter.x,
+            circleCenter.y,
+            radiusInPx,
+            Paint().apply {
+                strokeWidth = scaleWidthInPx
+                color = android.graphics.Color.WHITE
+                style = Paint.Style.STROKE
+                setShadowLayer(
+                    60f,
+                    0f,
+                    0f,
+                    ColorOld.argb(50,0,0,0)
+                )
+            }
+        )
+    }
+}
+
+private fun drawScaleWeightLines(
+    minWeight: Int,
+    maxWeight: Int,
+    initialWeight: Int,
+    angle : Float,
+    scaleStyle: ScaleStyle,
+    outerRadius : Float,
+    circleCenter: Offset,
+    drawScope: DrawScope,
+    nativeCanvas: Canvas
+) {
+
+    drawScope.apply {
+        for(i in minWeight..maxWeight) {
+
+            // TODO "angle" what for?
+            val angleInDegrees = i - initialWeight + angle - 90
+
+            // angle needed in radians for "sin" & "cos" kotlin functions
+            // this angle represents how inclined the line is to the center of the scale (lines representing the weight)
+            val angleInRadians = angleInDegrees * (PI / 180).toFloat() // angle multiplied by "(PI / 180)" converts the value from degrees to radians
+
+            val lineType = when {
+                i % 10 == 0 -> LineType.TenStepLine
+                i % 5 == 0 -> LineType.FiveStepLine
+                else -> LineType.SingleStepLine
+            }
+
+            val lineLength = when(lineType) {
+                is LineType.TenStepLine -> scaleStyle.tenStepLineLength.toPx()
+                is LineType.FiveStepLine -> scaleStyle.fiveStepLineLength.toPx()
+                is LineType.SingleStepLine -> scaleStyle.oneStepLineLength.toPx()
+            }
+
+            val lineColor = when(lineType) {
+                is LineType.TenStepLine -> scaleStyle.tenStepLineColor
+                is LineType.FiveStepLine -> scaleStyle.fiveStepLineColor
+                is LineType.SingleStepLine -> scaleStyle.oneStepLineColor
+            }
+
+            //(x,y) coordinates for the start of the lines
+            val lineStart = Offset(
+                x = (outerRadius - lineLength) * cos(angleInRadians) + circleCenter.x,
+                y = (outerRadius - lineLength) * sin(angleInRadians) + circleCenter.y
+            )
+
+            //(x,y) coordinates for the end of the lines, touching the outer edge of the scale
+            val lineEnd = Offset(
+                x = outerRadius * cos(angleInRadians) + circleCenter.x, // takes (0,0) according to video
+                y = outerRadius * sin(angleInRadians) + circleCenter.y
+            )
+
+            drawLine(
+                color = lineColor,
+                start = lineStart,
+                end = lineEnd,
+                strokeWidth = 1.dp.toPx()
+            )
+
+            //-------------------------------- weight line numbers -------------------------------//
+
+            if(lineType is LineType.TenStepLine) {
+                val marginIndicatorToNumber = 5.dp.toPx()
+                val textRadius = outerRadius - lineLength - marginIndicatorToNumber - scaleStyle.textSize.toPx()
+                val numberCoordinates = Offset(
+                    x = textRadius * cos(angleInRadians) + circleCenter.x,
+                    y = textRadius * sin(angleInRadians) + circleCenter.y
+                )
+
+                nativeCanvas.apply {
+                    drawText(
+                        abs(i).toString(),
+                        numberCoordinates.x,
+                        numberCoordinates.y,
+                        Paint().apply {
+                            textSize = scaleStyle.textSize.toPx()
+                            textAlign = Paint.Align.CENTER
+                        }
+                    )
+                }
+
+            }
+
+        }
+    }
+
 
 }
 
 data class ScaleStyle(
     val scaleWidth: Dp = 100.dp,
     val radius: Dp = 550.dp,
-    val normalLineColor: Color = Color.LightGray,
+    val oneStepLineColor: Color = Color.LightGray,
     val fiveStepLineColor: Color = Color.Green,
-    val tenStepLineColor: Color = Color.Black
+    val tenStepLineColor: Color = Color.Black,
+    val oneStepLineLength: Dp = 15.dp,
+    val fiveStepLineLength: Dp = 30.dp,
+    val tenStepLineLength: Dp = 45.dp,
+    val scaleIndicatorColor: Color = Color.Green,
+    val scaleIndicatorLength : Dp = 60.dp,
+    val textSize: TextUnit = 18.sp
 )
+
+sealed class LineType {
+    object SingleStepLine : LineType()
+    object FiveStepLine : LineType()
+    object TenStepLine : LineType()
+}
